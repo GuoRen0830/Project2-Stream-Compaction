664ea947cf83d1fa59b4faebe86a5acba403ce3f
ziziyang <ziziyang@tencent.com>
2026-09-14 20:50:17 -0400

    Finished CPU Part

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -19,7 +19,12 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            
+            odata[0] = 0;
+            for (int i = 1; i < n; ++i) {
+                odata[i] = odata[i - 1] + idata[i - 1];
+            }
+
             timer().endCpuTimer();
         }
 
@@ -30,9 +35,17 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+            
+            int count = 0;
+            for (int i = 0; i < n; ++i) {
+                if (idata[i] != 0) {
+                    odata[count] = idata[i];
+                    ++count;
+                }
+            }
+
             timer().endCpuTimer();
-            return -1;
+            return count;
         }
 
         /**
@@ -41,10 +54,32 @@ namespace StreamCompaction {
          * @returns the number of elements remaining after compaction.
          */
         int compactWithScan(int n, int *odata, const int *idata) {
-            timer().startCpuTimer();
-            // TODO
-            timer().endCpuTimer();
-            return -1;
+            //timer().startCpuTimer();
+
+            // Map bools
+            int* bools = new int[n];
+            for (int i = 0; i < n; ++i) {
+                bools[i] = idata[i] != 0 ? 1 : 0;
+            }
+            
+            // Scan
+            int* indices = new int[n];
+            scan(n, indices, bools);
+
+            // Scatter
+            for (int i = 0; i < n; ++i) {
+                if (bools[i]) {
+                    odata[indices[i]] = idata[i];
+                }
+            }
+
+            int count = indices[n - 1] + bools[n - 1];
+
+            delete[] bools;
+            delete[] indices;
+
+            //timer().endCpuTimer();
+            return count;
         }
     }
 }
